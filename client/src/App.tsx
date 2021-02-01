82bfd21ffb1cf5fee742d1a9d41ee669d8ae9101
hannesschaletzky <hannes.schaletzky@gmx.de>
2021-02-01 23:42:03 +0100

    refactored async class component to functional

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,10 @@ import st from './App.module.scss';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import Tweet from 'components/match/tweet/Tweet'
-
 import Root_Header from 'components/root/Header'
 import Landing from 'components/pages/Landing'
 
-import Matchroom from 'components/match/Match'
+import Match from 'components/match/Match'
 import Setup from 'components/match/Setup'
 import Mockup from 'components/match/Mockup'
 
@@ -24,91 +22,41 @@ import NotFound from 'components/pages/errorpages/NotFound'
 
 class App extends Component {
   state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.data.length }))
-      .catch(err => console.log(err));
-  }
-  
-  callApi = async () => {
-    const response = await fetch('/api/users');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    
-    return body;
-  };
-  
-  handleSubmit = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    
-    this.setState({ responseToPost: body });
   };
   
 render() {
-    return (
-      <div>
-        <div className={st.App}>
-          <p>{this.state.response}</p>
-          <form onSubmit={this.handleSubmit}>
-            <p>
-              <strong>Post to Server:</strong>
-            </p>
-            <input
-              type="text"
-              value={this.state.post}
-              onChange={e => this.setState({ post: e.target.value })}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.responseToPost}</p>
-        </div>
-        <div className={st.Root_Background}>
+  return (
+    <div className={st.Root_Background}>
 
-          <div className={st.Root_Header}>
-            <Root_Header/>
-          </div>
-          <div className ={st.Root_FlexboxWrapper}>
-            <div className={st.Root_Content}>
-              <BrowserRouter>
-                <Switch>
-                  <Route exact path="/" component={Landing}/>
-
-                  <Route exact path="/start" component={Landing}/>
-                  <Route exact path="/tweet" component={Tweet}/>
-                  <Route exact path="/legal" component={Legal}/>
-                  <Route exact path="/about" component={About}/>
-                  <Route exact path="/donate" component={Donate}/>
-                  <Route exact path="/credits" component={Credits}/>
-
-                  <Route exact path="/match/setup" component={Setup}/> 
-                  <Route exact path="/match/mockup" component={Mockup}/> 
-                  <Route path="/match/:id" component={Matchroom}/>
-
-                  <Route component={NotFound} /* final route for 404 not found *//>
-                </Switch>
-              </BrowserRouter>
-            </div>
-            <div className={st.Root_Footer}>
-              <Root_Footer/>
-            </div>
-          </div>
-          
-        </div>
-
+      <div className={st.Root_Header}>
+        <Root_Header/>
       </div>
+      <div className ={st.Root_FlexboxWrapper}>
+        <div className={st.Root_Content}>
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={Landing}/>
+
+              <Route exact path="/start" component={Landing}/>
+              <Route exact path="/legal" component={Legal}/>
+              <Route exact path="/about" component={About}/>
+              <Route exact path="/donate" component={Donate}/>
+              <Route exact path="/credits" component={Credits}/>
+
+              <Route exact path="/match/mockup" component={Mockup}/> 
+              <Route path="/match/setup/:id" component={Setup}/> 
+              <Route path="/match/:id" component={Match}/>
+
+              <Route component={NotFound} /* final route for 404 not found *//>
+            </Switch>
+          </BrowserRouter>
+        </div>
+        <div className={st.Root_Footer}>
+          <Root_Footer/>
+        </div>
+      </div>
+      
+    </div>
     );
   }
 }
--- client/src/components/match/Setup.tsx
+++ client/src/components/match/Setup.tsx
@@ -1,18 +1,127 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
+
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import st from './Setup.module.scss'
 
+
+export default function Match_Setup() {
+    const [response, setResponse] = useState("");
+    const [post, setPost] = useState("");
+    const [responseToPost, setresponseToPost] = useState("");
+    const { id } = useParams<Record<string, string | undefined>>()
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        callApi()
+        .then(res => setResponse(res.data.length))
+        .catch(err => console.log(err));
+    });
+
+    const callApi = async () => {
+        const response = await fetch('/api/users');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        
+        return body;
+    };
+  
+    const handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const response = await fetch('/api/world', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ post: {post} }),
+        });
+        const body = await response.text();
+        
+        setresponseToPost(body)
+    };
+
+  return (
+    <div >
+        <div className={st.App}>
+        <p>{response}</p>
+        <form onSubmit={handleSubmit}>
+            <p>
+            <strong>Post to Server from match with id: {id}</strong>
+            </p>
+            <input
+            type="text"
+            value={post}
+            onChange={e => setPost(e.target.value)}
+            />
+            <button type="submit">Submit</button>
+        </form>
+        <p>{responseToPost}</p>
+        </div>
+    </div>
+  );
+}
+
+
+/*
 class Matchroom_Setup extends Component <any, any> {
+
+    id = useParams<Record<string, string | undefined>>()
 
     constructor(props: any) {
         super(props);
         this.state = {
+            response: '',
+            post: '',
+            responseToPost: '',
         };
     }
+
+    componentDidMount() {
+        this.callApi()
+        .then(res => this.setState({ response: res.data.length }))
+        .catch(err => console.log(err));
+    }
+  
+    callApi = async () => {
+        const response = await fetch('/api/users');
+        const body = await response.json();
+        if (response.status !== 200) throw Error(body.message);
+        
+        return body;
+    };
+  
+    handleSubmit = async (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        const response = await fetch('/api/world', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ post: this.state.post }),
+        });
+        const body = await response.text();
+        
+        this.setState({ responseToPost: body });
+    };
     
     render() { 
         return (  
             <div >
-                There will be the matchroom setup
+                <div className={st.App}>
+                <p>{this.state.response}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <p>
+                    <strong>Post to Server:</strong>
+                    </p>
+                    <input
+                    type="text"
+                    value={this.state.post}
+                    onChange={e => this.setState({ post: e.target.value })}
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+                <p>{this.state.responseToPost}</p>
+                </div>
             </div>
         );
     }
@@ -20,4 +129,4 @@ class Matchroom_Setup extends Component <any, any> {
 }
 export default Matchroom_Setup;
 
-
+*/
