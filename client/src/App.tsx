7ac135bc8ca6fb81fad3a32e0ae5624ccc2b0afe
hannesschaletzky <hannes.schaletzky@gmx.de>
2021-02-01 15:31:00 +0100

    working twitter api calls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,12 +18,12 @@ class App extends Component {
   
   componentDidMount() {
     this.callApi()
-      .then(res => this.setState({ response: res.express }))
+      .then(res => this.setState({ response: res.id }))
       .catch(err => console.log(err));
   }
   
   callApi = async () => {
-    const response = await fetch('/api/tweets');
+    const response = await fetch('/api/tweets/new');
     const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 let unirest = require('unirest');
+const oauthSignature = require('oauth-signature');
+const { sign } = require('crypto');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,8 +20,11 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/tweets', (req, res) => {
 
-  const { SERVER_TWITTER_API_BEARER } = require('./config'); //get env var from config.js
-  //console.log(SERVER_TWITTER_API_BEARER)
+  //get env vars -> @@REMOVE CONSOLE OUTPUT
+  const dotenv = require('dotenv');
+  dotenv.config(); 
+  const SERVER_TWITTER_API_BEARER = process.env.SERVER_TWITTER_API_BEARER
+  console.log('SERVER_TWITTER_API_BEARER: ' + SERVER_TWITTER_API_BEARER)
 
   unirest('GET', 'https://api.twitter.com/2/tweets?ids=1345396853815312385&tweet.fields=public_metrics,created_at&expansions=attachments.media_keys&media.fields=duration_ms,height,media_key,public_metrics,type,url,width')
     .headers({
@@ -27,10 +32,115 @@ app.get('/api/tweets', (req, res) => {
     })
     .end(function (response) { 
       if (response.error) throw new Error(response.error); 
-      console.log(response.raw_body);
+      console.log(response);
       res.send({ express: response.raw_body})
     });
 });
+
+app.get('/api/tweets/new', async(req, res) => {
+
+  //get env vars -> @@REMOVE CONSOLE OUTPUT
+  const dotenv = require('dotenv');
+  dotenv.config(); 
+  const SERVER_TWITTER_API_Key = process.env.SERVER_TWITTER_API_Key
+  const SERVER_TWITTER_API_Secret = process.env.SERVER_TWITTER_API_Secret
+  const SERVER_TWITTER_Access_Token = process.env.SERVER_TWITTER_Access_Token
+  const SERVER_TWITTER_Access_Token_Secret = process.env.SERVER_TWITTER_Access_Token_Secret
+  const SERVER_TWITTER_API_BEARER = process.env.SERVER_TWITTER_API_BEARER
+  console.log('SERVER_TWITTER_API_Key: ' + SERVER_TWITTER_API_Key)
+  console.log('SERVER_TWITTER_API_Secret: ' + SERVER_TWITTER_API_Secret)
+  console.log('SERVER_TWITTER_Access_Token: ' + SERVER_TWITTER_Access_Token)
+  console.log('SERVER_TWITTER_Access_Token_Secret: ' + SERVER_TWITTER_Access_Token_Secret)
+  console.log('SERVER_TWITTER_API_BEARER: ' + SERVER_TWITTER_API_BEARER)
+
+
+  const Twitter = require('twitter-v2');
+  const client = new Twitter({
+    consumer_key: SERVER_TWITTER_API_Key,
+    consumer_secret: SERVER_TWITTER_API_Secret,
+    bearer_token: SERVER_TWITTER_API_BEARER
+  });
+  
+  var params = {
+    ids: '1345396853815312385',
+    'tweet.fields': 'public_metrics,created_at',
+    'expansions': 'attachments.media_keys',
+    'media.fields': 'duration_ms,height,media_key,public_metrics,type,url,width'
+  };
+  const { data } = await client.get('tweets', params);
+  console.log(data);
+
+  res.send({ express: data})
+});
+
+
+app.get('/api/users/new', (req, res) => {
+
+  //get env vars @@REMOVE CONSOLE OUTPUT
+  const dotenv = require('dotenv');
+  dotenv.config(); 
+  const SERVER_TWITTER_API_Key = process.env.SERVER_TWITTER_API_Key
+  const SERVER_TWITTER_API_Secret = process.env.SERVER_TWITTER_API_Secret
+  const SERVER_TWITTER_Access_Token = process.env.SERVER_TWITTER_Access_Token
+  const SERVER_TWITTER_Access_Token_Secret = process.env.SERVER_TWITTER_Access_Token_Secret
+  console.log('SERVER_TWITTER_API_Key: ' + SERVER_TWITTER_API_Key)
+  console.log('SERVER_TWITTER_API_Secret: ' + SERVER_TWITTER_API_Secret)
+  console.log('SERVER_TWITTER_Access_Token: ' + SERVER_TWITTER_Access_Token)
+  console.log('SERVER_TWITTER_Access_Token_Secret: ' + SERVER_TWITTER_Access_Token_Secret)
+
+  var Twitter = require('twitter');
+  var client = new Twitter({
+    consumer_key: SERVER_TWITTER_API_Key,
+    consumer_secret: SERVER_TWITTER_API_Secret,
+    access_token_key: SERVER_TWITTER_Access_Token,
+    access_token_secret: SERVER_TWITTER_Access_Token_Secret
+  });
+
+  /*
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+  });
+  */
+
+  /*
+  var params = {
+    q: 'Götze',
+    count: 1
+  };
+  client.get('users/search', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+    else {
+      console.log(error);
+    }
+  });
+*/
+  var params = {
+    user_id: '342599843',
+    trim_user: true
+  };
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+      console.log(tweets);
+    }
+    else {
+      console.log(error);
+    }
+  });
+
+  res.send({ express: "THERE WILL BE NEW USERS: "})
+});
+
+
+
+
+
+
+
 
 app.post('/api/world', (req, res) => {
   console.log(req.body);
@@ -38,6 +148,11 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
 });
+
+
+
+
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
