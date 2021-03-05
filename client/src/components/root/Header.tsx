e14092700df18a40531164e13d83e1521bcf494d
hannesschaletzky <hannes.schaletzky@gmx.de>
2021-03-05 01:35:54 +0100

    removed Menu

--- client/src/components/root/Header.tsx
+++ client/src/components/root/Header.tsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import st from './Header.module.scss'
 
-import Menu from '../menu/Menu'
-
 class Root_Header extends Component {
 
     constructor(props: any) {
@@ -14,9 +12,6 @@ class Root_Header extends Component {
     render() { 
         return (  
             <div>
-                <div className = {st.Menu}>
-                    <Menu/>
-                </div>
                 <div className={st.Headline_Container}>
                     <a className={st.Headline} href="/start">citefight</a>
                 </div>
